f55dbbe7ac26ebc1c8f5c25c0300156463be2a27
ZER0-sh1kh4 <56764772+ZER0-sh1kh4@users.noreply.github.com>
2026-01-30 10:05:28 +0530

    Create FileI\O.cs

---
+++ Assesment/FileI\O.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string inputFile = "log.txt";
+        string outputFile = "error.txt";
+        string[] lines = File.ReadAllLines(inputFile);
+        using (StreamWriter writer = new StreamWriter(outputFile))
+        {
+            foreach (string line in lines)
+            {
+                if (line.Contains("ERROR"))
+                {
+                    writer.WriteLine(line);
+                }
+            }
+        }
+        Console.WriteLine("ERROR logs saved to error.txt");
+    }
+}
